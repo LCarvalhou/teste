130f59c43ad587746be1a6bdb785f388d18f9aa8
Leonardo Carvalho <leocarvalhoo97@gmail.com>
2026-03-13 11:21:49 -0400

    att cod

---
+++ Inicio/comentario.js
@@ -0,0 +1,4 @@
+ console.log("passo 1");    // teste ! 
+console.log("passo 2");    // teste ! 
+console.log("passo 3");    // teste ! 
+
---
+++ Inicio/primeiro.js
@@ -0,0 +1,2 @@
+console.log("passo 1");
+console.log("passo 1");
